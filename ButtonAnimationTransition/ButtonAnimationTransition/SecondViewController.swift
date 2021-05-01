6de8d9d5b1e8fca36b66197a66197337484847b1
jaeyong <maxwodyd98@gmail.com>
2021-05-02 00:47:10 +0900

    create: button animate

---
+++ ButtonAnimationTransition/ButtonAnimationTransition/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  ButtonAnimationTransition
+//
+//  Created by 이재용 on 2021/05/01.
+//
+
+import TransitionButton
+import UIKit
+
+class SecondViewController: CustomTransitionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
--- ButtonAnimationTransition/ButtonAnimationTransition/ViewController.swift
+++ ButtonAnimationTransition/ButtonAnimationTransition/ViewController.swift
@@ -4,16 +4,65 @@
 //
 //  Created by 이재용 on 2021/05/01.
 //
-
+import TransitionButton
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let button = TransitionButton(frame: CGRect(x: 100, y: 100, width: 100, height: 40))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(button)
+        button.backgroundColor = .red
+        button.setTitle("butoon", for: .normal)
+        button.cornerRadius = 20
+        button.spinnerColor = .white
+        button.addTarget(self, action: #selector(animateTouchDown(_:)), for: .touchDown)
+        button.addTarget(self, action: #selector(animateTouchUp(_:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(animateTouchUp(_:)), for: .touchUpOutside)
+        
+//        button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
     }
+    
+    @objc func buttonAction(_ button: TransitionButton) {
+        animateView(button)
+        button.startAnimation()
+        let qualityOfServiceClass = DispatchQoS.QoSClass.background
+        let backgroundQueue = DispatchQueue.global(qos: qualityOfServiceClass)
+        backgroundQueue.async {
+            sleep(3)
+            
+            DispatchQueue.main.async {
+                button.stopAnimation(animationStyle: .expand, completion: {
+                    let secondVC = self.storyboard?.instantiateViewController(identifier: "SecondViewController") as! SecondViewController
+                    self.present(secondVC, animated: true, completion: nil)
+                })
+            }
+        }
+    }
+    
+    @objc func animateTouchDown(_ viewToAnimate: UIView) {
+        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: .curveEaseIn) {
+            viewToAnimate.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
+        }
+    }
+    
+    @objc func animateTouchUp(_ viewToAnimate: UIView) {
+        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn) {
+            viewToAnimate.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+    }
+    
+    fileprivate func animateView(_ viewToAnimate: UIView) {
+        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: .curveEaseIn) {
+            viewToAnimate.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
+                viewToAnimate.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }, completion: nil)
+        }
 
-
+    }
 }
-
